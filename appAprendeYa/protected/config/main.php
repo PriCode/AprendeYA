a1c680c74db4ee3336f695ca242af450b976c4e9
Javier Villegas Herrera <javier.viherrera@gmail.com>
2014-12-09 13:00:39 -0500

    pass update

--- appAprendeYa/protected/config/main.php
+++ appAprendeYa/protected/config/main.php
@@ -57,7 +57,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=aprendeya',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '123456',
+			'password' => '.-dogzilla.-',
 			'charset' => 'utf8',
 		),
 		
