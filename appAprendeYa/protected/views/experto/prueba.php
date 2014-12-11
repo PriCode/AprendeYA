7bf33be234d4cf51ba3f5b39fab9351f3cb46124
Jonaxz <gjonaxz@gmail.com>
2014-12-10 23:23:48 -0500

    interfaz v1.2

--- appAprendeYa/protected/views/experto/prueba.php
+++ appAprendeYa/protected/views/experto/prueba.php
@@ -1,13 +1,55 @@
-<table>
-<tr>
-
-<td><b>Experto</b></td><td><b>Calificación</b></td>
-</tr>
-<?php 
-foreach($a as $key){
-		foreach($key as $b){
-		echo "<tr><td>".$b['usuario']."</td><td>".$b['calificacion']."</td></tr>";}
+<?php
+	class Item2{
+	public $expertp;
+	public $calificacion;
 	}
 
+$items= array();
+$i=0;
+
+foreach($a as $key){
+	foreach($key as $b){
+		$mark = new Item2();
+		$mark->id=++$i;
+	    $mark->experto = $b['usuario'];
+	    $mark->calificacion = $b['calificacion'];    
+	    $items[]=$mark;
+	}	
+}
+    // $gridDataProvider
+   
+     
+    $gridDataProvider = new CArrayDataProvider($items);
+     
+    // $gridColumns
+    $gridColumns = array(
+    array('name'=>'id', 'header'=>'#', 'htmlOptions'=>array('style'=>'width: 60px')),
+    array('name'=>'experto', 'header'=>'Experto'),
+    array('name'=>'calificacion', 'header'=>'Calificacion'),    
+    array(
+    'htmlOptions' => array('nowrap'=>'nowrap'),
+    'class'=>'booster.widgets.TbButtonColumn',
+    'viewButtonUrl'=>null,
+    'updateButtonUrl'=>null,
+    'deleteButtonUrl'=>null,
+    )
+    );
+    ?>
+
+<?php
+
+    $this->widget('booster.widgets.TbGridView', array(
+    'type' => 'striped bordered condensed',
+    'dataProvider' => $gridDataProvider,
+    'template' => "{items}",    
+    'columns' => $gridColumns,
+    ));
+//'filter' => $person->search(),
 ?>
-</table>
+
+
+
+
+
+
+
--- appAprendeYa/protected/views/matricula/prueba.php
+++ appAprendeYa/protected/views/matricula/prueba.php
@@ -1,11 +1,55 @@
-<table>
-<tr>
 
-<td><b>Alumno</b></td><td><b>Sesión</b></td><td><b>Descripción de la Sesión</b></td>
-</tr>
-<?php 
+<?php
+	class Item{
+
+	public $alumno;
+	public $sesion;
+	public $descripcion;
+
+	}
+
+$items= array();
+$i=0;
 foreach($a as $key){
-	echo "<tr><td>".$key['usuario']."</td><td>".$key['sesion']."</td><td>".$key['desc_sesion']."</td></tr>";
+	$mark = new Item();
+	$mark->id=++$i;
+    $mark->alumno = $key['usuario'];
+    $mark->sesion = $key['sesion'];
+    $mark->descripcion =$key['desc_sesion'];   
+	$items[]=$mark;
 }
+    // $gridDataProvider
+   
+     
+    $gridDataProvider = new CArrayDataProvider($items);
+     
+    // $gridColumns
+    $gridColumns = array(
+    array('name'=>'id', 'header'=>'#', 'htmlOptions'=>array('style'=>'width: 60px')),
+    array('name'=>'alumno', 'header'=>'Alumno'),
+    array('name'=>'sesion', 'header'=>'Sesión'),
+    array('name'=>'descripcion', 'header'=>'Descripción'),    
+    array(
+    'htmlOptions' => array('nowrap'=>'nowrap'),
+    'class'=>'booster.widgets.TbButtonColumn',
+    'viewButtonUrl'=>null,
+    'updateButtonUrl'=>null,
+    'deleteButtonUrl'=>null,
+    )
+    );
+     
+    
+
+    ?>
+
+
+<?php
+
+    $this->widget('booster.widgets.TbGridView', array(
+    'type' => 'striped bordered condensed',
+    'dataProvider' => $gridDataProvider,
+    'template' => "{items}",    
+    'columns' => $gridColumns,
+    ));
+//'filter' => $person->search(),
 ?>
-</table>
